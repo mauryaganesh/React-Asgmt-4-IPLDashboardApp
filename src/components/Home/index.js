2d03af6445d695da5b130fd474d9eac62e88029e
Ganesh Maurya <starganesh.maurya@gmail.com>
2024-05-29 01:08:34 +0000

    Files Upload

---
+++ src/components/Home/index.js
@@ -0,0 +1,69 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import TeamCard from '../TeamCard'
+
+import './index.css'
+
+class Home extends Component {
+  state = {
+    teamsData: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getTeamsData()
+  }
+
+  getTeamsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const data = await response.json()
+    const formattedData = data.teams.map(eachTeam => ({
+      name: eachTeam.name,
+      id: eachTeam.id,
+      teamImageUrl: eachTeam.team_image_url,
+    }))
+
+    this.setState({teamsData: formattedData, isLoading: false})
+  }
+
+  renderLoader = () => (
+    //   Add the testid="loader" to pass the test case for eg. <div testid="loader">
+    <div className="loader-container">
+      <Loader type="Oval" color="#ffffff" height={50} />
+    </div>
+  )
+
+  renderTeamsList = () => {
+    const {teamsData} = this.state
+
+    return (
+      <ul className="teams-list">
+        {teamsData.map(team => (
+          <TeamCard key={team.id} teamDetails={team} />
+        ))}
+      </ul>
+    )
+  }
+
+  render() {
+    const {isLoading} = this.state
+    return (
+      <div className="home-route-container">
+        <div className="teams-list-container">
+          <div className="ipl-dashboard-heading-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+              alt="ipl logo"
+              className="ipl-logo"
+            />
+            <h1 className="ipl-dashboard-heading">IPL Dashboard</h1>
+          </div>
+          {isLoading ? this.renderLoader() : this.renderTeamsList()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,40 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const TeamCard = props => {
+  const {teamDetails} = props
+  const {name, id, teamImageUrl} = teamDetails
+  return (
+    <li className="team-item">
+      <Link to={`/team-matches/${id}`} className="link">
+        <img src={teamImageUrl} alt={name} className="team-logo" />
+        <p className="team-name">{name}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default TeamCard
+
+/*
+Used this code to test by putting the Link Outside of the <li> element
+
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const TeamCard = props => {
+  const {teamDetails} = props
+  const {name, id, teamImageUrl} = teamDetails
+  return (
+    <Link to={`/team-matches/${id}`} className="team-item">
+      <li className="link">
+        <img src={teamImageUrl} alt={name} className="team-logo" />
+        <p className="team-name">{name}</p>
+      </li>
+    </Link>
+  )
+}
+
+export default TeamCard
+
+*/
